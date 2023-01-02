8839fa5416aa62b885429be3c6874996bdb8d97a
Alejandro <lopalejandro@gmail.com>
2023-01-02 11:23:36 -0300

    Initial commit

---
+++ test_polimorfismo.py
@@ -0,0 +1,14 @@
+from Empleado import Empleado
+from Gerente import Gerente
+
+
+def imprimir_detalles(objeto):
+    print(objeto)
+    print(type(objeto))
+
+
+empleado = Empleado('Juan', 5000)
+imprimir_detalles(empleado)
+
+gerente = Gerente('Karla', 6000, 'sistemas')
+imprimir_detalles(gerente)
